00f10cb3715ba9e98d00333e5dbec114084770a4
Junyoung2Yang <2271134@hansung.ac.kr>
2024-09-01 01:36:06 +0900

    exposedHeaders 추가

--- src/main/java/com/covelopment/coveloper/config/WebConfig.java
+++ src/main/java/com/covelopment/coveloper/config/WebConfig.java
@@ -10,9 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000") // 배포시 도메인 추가
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .exposedHeaders("Authorization")
+                .allowCredentials(true)
+                .maxAge(3600); // 캐시 기간 설정
     }
 }
